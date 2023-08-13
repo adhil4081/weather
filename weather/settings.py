8f01708c082b889c54c64f19b67e108b328d9cde
Adhil <129657575+adhil4081@users.noreply.github.com>
2023-08-13 03:08:39 -0700

    Update settings.py

--- weather/settings.py
+++ weather/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-0m)m(dxvwn_&+!rqjjg@ku&5gt$tij*1)57e6g6f=^d2o1-)b^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://weather-app-hwuq.onrender.com'
+]
 
 
 # Application definition
